38ef7ae56c5c617e3794c61f2c6f633eb25adaca
clairecao0420 <claire.cao0420@gmail.com>
2025-07-31 17:31:16 +0000

    hi

--- conect four.py
+++ conect four.py
@@ -1,10 +1,27 @@
 board=[['-','-','-','-','-','-','-'],['-','-','-','-','-','-','-'],['-','-','-','-','-','-','-'],['-','-','-','-','-','-','-'],['-','-','-','-','-','-','-'],['-','-','-','-','-','-','-']]
 win=False
+turn='o'
+colums=[0,0,0,0,0,0,0]
 while win==False:
     for i in range (6):
         print(board[i])
     colum=int(input('pick a colum.'))
-    turn='o'
-    colums=[0,0,0,0,0,0,0]
-    board[5-colums[colum]][colum]='o'
+    board[5-colums[colum]][colum]=turn
     colums[colum]+=1
+    for i in range(3):
+        for j in range(7):
+            if board[i][j]==turn and board[i+1][j]==turn and board[i+2][j]==turn and board[i+3][j]==turn:
+                win=True
+                for i in range (6):
+                    print(board[i])
+                print(turn +' won')
+    for i in range(4):
+        if board[5][i]==turn and board[5][i]==turn and board[5][i]==turn and board[5][i]:            
+            print(turn +' won')
+    if colum>6:
+        colum=6
+    if turn=='o':
+        turn='x'
+    else:
+        turn='o'
+        
